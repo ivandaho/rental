1510405e3218138c374c53b440ae2f36c7f0c504
ivandaho <ivanho_yo@hotmail.com>
2015-11-12 15:57:01 -0500

    Moved required script files into js/req

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 // App Module: the name AngularStore matches the ng-app attribute in the main <html> tag
 // the route provides parses the URL and injects the appropriate partial page
-var storeApp = angular.module('AngularStore', []).
+var storeApp = angular.module('AngularStore', ['ngRoute']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when('/store', {
